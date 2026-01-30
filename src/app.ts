b9b500f4bbbc86522134ba4d51f6294f31e3b026
paulobr4z <paulobrazaraujo@gmail.com>
2026-01-30 20:40:37 -0300

    feat: cria rota de cadastro de usuários

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-import fastify from "fastify"
-import { usersRoutes } from "./routes/users.routes.js"
-import { mealsRoutes } from "./routes/meals.routes.js"
+import fastify from 'fastify'
+import { usersRoutes } from './routes/users.routes.js'
+import { mealsRoutes } from './routes/meals.routes.js'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
 
+app.register(fastifyCookie)
+
 app.register(usersRoutes, {
-  prefix: "users"
+  prefix: 'users',
 })
 
 app.register(mealsRoutes, {
-  prefix: "meals"
+  prefix: 'meals',
 })
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,40 @@
-import type { FastifyInstance } from "fastify";
+import type { FastifyInstance } from 'fastify'
+import z from 'zod'
+import { db } from '../database.js'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get("/", () => {
-    return "users"
-  })  
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.email(),
+    })
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    const { name, email } = createUserBodySchema.parse(request.body)
+
+    const userByEmail = await db('users').where({ email }).first()
+
+    if (userByEmail) {
+      return reply.status(400).send({ message: 'User already exists' })
+    }
+
+    await db('users').insert({
+      id: crypto.randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
 }
